042a6aeb551257d9fda57b8e7331cb3e11b52ed1
Ivan <ivan@it-amigo.se>
2017-02-06 11:30:02 +0100

    Added moment to karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function(config) {
     },
     files: [
       './node_modules/angular/angular.js',
+      './node_modules/moment/moment.js',
       './node_modules/angular-moment/angular-moment.js',
       './node_modules/angular-animate/angular-animate.js',
       './node_modules/angular-ui-router/release/angular-ui-router.js',
