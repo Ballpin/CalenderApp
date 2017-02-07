b4e26de3b4af93eeea4d6a7ca84759c33eacb8dc
Ivan <ivan@it-amigo.se>
2017-02-07 14:17:00 +0100

    Fixed logout url regex

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     # Regular Login Urls
     url(r'^login/?$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout/?$', auth_views.logout, name='logout'),
     url(r'^password_change/$', auth_views.password_change, name='password_change'),
     url(r'^password_change/done/$', auth_views.password_change_done, name='password_change_done'),
     url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
